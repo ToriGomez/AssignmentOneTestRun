1033f843acc1e4fffa15f6ab6f4be6d1e2876c4d
Victoria Gomez <77286079+ToriGomez@users.noreply.github.com>
2021-01-15 08:18:22 -0700

    Introduction Hello.

--- AssignmentOneTestRun/AssignmentOneTestRun.vb
+++ AssignmentOneTestRun/AssignmentOneTestRun.vb
@@ -2,6 +2,12 @@
 
     Sub Main()
 
+        Console.WriteLine("HELLO WORLD")
+        Console.WriteLine("WELCOME TO 2021")
+        Console.WriteLine("MAY THE ODDS EVER BE EVER IN YOUR FAVOR")
+
+        Console.ReadLine()
+
     End Sub
 
 End Module
